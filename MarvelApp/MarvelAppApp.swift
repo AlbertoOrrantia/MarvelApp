2f186868ef9a6f023776db2f3e45587d98b1f050
Alberto Orrantia <ricco.albert@gmail.com>
2024-10-01 15:06:44 -0700

    Initial Commit

---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Alberto Orrantia on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
