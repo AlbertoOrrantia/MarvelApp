e3a0a1f9df1af862705e456bf2039e0939214c10
Alberto Orrantia <ricco.albert@gmail.com>
2024-10-04 01:56:19 -0700

    Update CharacterViewModel
    Added Pagination

--- ViewModel/CharacterViewModel.swift
+++ ViewModel/CharacterViewModel.swift
@@ -12,19 +12,27 @@ class CharacterViewModel: ObservableObject {
     @Published var isLoading = true
     @Published var errorMessage: String?
     @Published var selectedCharacter: Character?
+    @Published var isFetchingMore = false
     
     private let apiClient = MarvelAPIDataClient()
+    private var offset = 0
+    private let limit = 20
 
     func fetchCharacters() {
-        apiClient.fetchCharacters { [weak self] result in
+        if isFetchingMore { return }
+        isFetchingMore = true
+        
+        apiClient.fetchCharacters(limit: limit, offset: offset) { [weak self] result in
             DispatchQueue.main.async {
                 if let result = result {
-                    self?.characters = result
+                    self?.characters.append(contentsOf: result)
+                    self?.offset += self?.limit ?? 0
                     self?.isLoading = false
                 } else {
                     self?.errorMessage = "Failed to load, Please try again later."
                     self?.isLoading = false
                 }
+                self?.isFetchingMore = false
             }
         }
     }
